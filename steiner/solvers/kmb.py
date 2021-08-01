6474c99c4c9c2cd055e4fe976c08c66f0bf1af9f
Dmitry Khripunkov <khripunkovDV@gmail.com>
2021-08-01 13:42:54 +0300

    KMB algorithm proof of concept (#2)
    * Exclude launch.json from gitignore
    
    * Add empty line to readme
    
    * Add formatting tools
    
    * Add test data
    
    * Add graph representation class
    
    * Create parser
    
    * Add entrypoint to test graph parsing
    
    * Add launch configuration
    
    * Run formatting and update readme
    
    * Remove comment section parsing from stpparser
    
    * Change data structures to use networkx library
    
    * Fix weight type from str to int while parsing graph edges
    
    * Implement KMB algorithm PoC using networkx lib
    
    * Adjust solver to return final tree
    
    * Add graph visualisation
    
    * Adjust graph visualization params
    
    * Change graph layout and add new launch configuration
    
    * Remove duplicate stp parser
    
    * Refactor KMB solver
    
    * Replace mst module import to func import

---
+++ steiner/solvers/kmb.py
@@ -0,0 +1,52 @@
+from typing import List, Tuple
+
+import networkx as nx
+from networkx.algorithms.tree.mst import minimum_spanning_tree
+
+from steiner.utils.graph import graph_weight_sum
+
+
+class SolverKMB:
+    def solve(self, graph: nx.Graph, terminals: List[int]) -> Tuple[nx.Graph, int]:
+        metric_closure_graph = self.get_induced_metric_closure(graph, terminals)
+
+        metric_closure_mst_graph = minimum_spanning_tree(metric_closure_graph)
+
+        unfolded_metric_closure_mst_graph = self.unfold_induced_metric_closure(graph, metric_closure_mst_graph)
+
+        steiner_tree = minimum_spanning_tree(unfolded_metric_closure_mst_graph)
+        steiner_tree_cost = graph_weight_sum(steiner_tree)
+
+        return steiner_tree, steiner_tree_cost
+
+    def get_induced_metric_closure(
+            self, graph: nx.Graph, terminals: List[int]
+        ) -> nx.Graph:
+        induced_metric_closure_graph = nx.Graph()
+
+        for term1 in terminals:
+            for term2 in terminals:
+                if term1 != term2:
+                    weight = nx.dijkstra_path_length(graph, term1, term2)
+                    induced_metric_closure_graph.add_edge(term1, term2, weight=weight)
+
+        return induced_metric_closure_graph
+
+    def unfold_induced_metric_closure(self, original_graph: nx.Graph, metric_closure_graph: nx.Graph) -> nx.Graph:
+        unfolded_graph = nx.Graph
+
+        for src, dest, weight in metric_closure_graph.edges.data("weight"):
+            corresponding_path = nx.dijkstra_path(original_graph, src, dest)
+            for node in corresponding_path:
+                try:
+                    next_node = corresponding_path[corresponding_path.index(node) + 1]
+                except IndexError:
+                    break
+
+                unfolded_graph.add_edge(
+                    node,
+                    next_node,
+                    weight=original_graph.get_edge_data(node, next_node)["weight"],
+                )
+        
+        return unfolded_graph
